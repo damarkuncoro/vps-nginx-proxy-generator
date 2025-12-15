a95a21a70041eca8a36f93ad0271f36d66241190
Damar Kuncoro <damarkuncoro@gmail.com>
2025-12-15 13:43:46 +0700

    feat: add app generator script for nginx deployment
    Add a bash script to automate nginx app deployment with docker-compose

---
+++ scripts/app-generator.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+read -p "🌍 Domain (example.com): " DOMAIN
+read -p "📧 SSL Email: " EMAIL
+
+APP_DIR="/opt/apps/$DOMAIN"
+mkdir -p $APP_DIR
+cd $APP_DIR
+
+cat <<EOF > docker-compose.yml
+version: "3.8"
+
+services:
+  app:
+    image: nginx
+    restart: always
+    environment:
+      - VIRTUAL_HOST=$DOMAIN,www.$DOMAIN
+      - LETSENCRYPT_HOST=$DOMAIN,www.$DOMAIN
+      - LETSENCRYPT_EMAIL=$EMAIL
+    networks:
+      - proxy
+
+networks:
+  proxy:
+    external: true
+EOF
+
+docker-compose up -d
+echo "🚀 $DOMAIN deployed"
